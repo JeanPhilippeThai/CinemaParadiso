955f1477be62eb19d2fba036e74c558ead17dfb2
Rodi <sasiroa@gmail.com>
2025-06-09 13:34:38 +0000

    test2

--- dbt_cinemaparadiso/models/cleaning/cleaning_cinema_tickets_sales.sql
+++ dbt_cinemaparadiso/models/cleaning/cleaning_cinema_tickets_sales.sql
@@ -2,4 +2,5 @@ SELECT
   Ticket_ID as rodrigo_ticket_id,
   age,
   Ticket_Price
-FROM {{ source('raw_cinema_tickets_sales', 'bfi_movies') }}
+FROM {{ source('raw_cinema_ticket_sales', 'bfi_movies') }}
+
