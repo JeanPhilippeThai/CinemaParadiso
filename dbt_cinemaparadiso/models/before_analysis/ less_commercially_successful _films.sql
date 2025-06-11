2436c5c228bd68408aa962175ab75d76438034f1
Farzaneh Ariyanpour <ariyanpour.farzaneh@gmail.com>
2025-06-11 12:11:34 +0000

    creating ba table

---
+++ dbt_cinemaparadiso/models/before_analysis/ less_commercially_successful _films.sql
@@ -0,0 +1,18 @@
+WITH avg_gross AS (
+  SELECT AVG(worldwide_gross) AS avg_worldwide_gross
+  FROM {{ ref('cleaned_enhanced_box_office') }}
+),
+filtered_movies AS (
+  SELECT 
+    ceb.title,
+    ceb.imdb_rating,
+    ceb.vote_count,
+    ceb.worldwide_gross,
+  FROM {{ ref('cleaned_enhanced_box_office') }} ceb
+  CROSS JOIN avg_gross
+  WHERE ceb.worldwide_gross < avg_gross.avg_worldwide_gross
+     OR ceb.imdb_rating > 8.5
+)
+SELECT *
+FROM filtered_movies
+ORDER BY imdb_rating DESC
