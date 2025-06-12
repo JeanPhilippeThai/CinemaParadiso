d8ccfa83a4c4f37704715a859be064de7cb4cd97
Farzaneh Ariyanpour <ariyanpour.farzaneh@gmail.com>
2025-06-12 17:24:44 +0000

    change source of less commercial view

--- dbt_cinemaparadiso/models/before_analysis/ba_less_commercially_successful_films.sql
+++ dbt_cinemaparadiso/models/before_analysis/ba_less_commercially_successful_films.sql
@@ -1,19 +1,19 @@
 WITH avg_gross AS (
   SELECT AVG(worldwide_gross) AS avg_worldwide_gross
-  FROM {{ ref('cleaned_enhanced_box_office') }}
+  FROM {{ ref('fa_total_cinema_box_office_join') }}
 ),
 filtered_movies AS (
   SELECT 
     ceb.title,
-    ceb.imdb_rating,
-    ceb.vote_count,
+    ceb.rating,
+    ceb.votes,
     ceb.worldwide_gross,
     ceb.weighted_score
-  FROM {{ ref('cleaned_enhanced_box_office') }} ceb
+  FROM {{ ref('fa_total_cinema_box_office_join') }} ceb
   CROSS JOIN avg_gross
   WHERE ceb.worldwide_gross < avg_gross.avg_worldwide_gross
-     OR ceb.imdb_rating > 8.5
+     OR ceb.rating > 8.5
 )
 SELECT *
 FROM filtered_movies
-ORDER BY imdb_rating DESC
+ORDER BY rating DESC
