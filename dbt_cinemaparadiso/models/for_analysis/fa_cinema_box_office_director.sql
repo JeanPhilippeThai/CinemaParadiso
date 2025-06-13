7269b62e515b82c80be293c437b24a4d875b41ee
Farzaneh Ariyanpour <ariyanpour.farzaneh@gmail.com>
2025-06-13 09:47:06 +0000

    adding director name to cinema

---
+++ dbt_cinemaparadiso/models/for_analysis/fa_cinema_box_office_director.sql
@@ -0,0 +1,32 @@
+with this as (
+select cinema.*,
+movie.director AS director,
+COALESCE(cinema.duration, movie.duration) AS duration
+FROM {{ ref('fa_total_cinema_box_office_joinn') }} AS cinema
+left join {{ ref('cleaning_movie_statistics') }} AS movie
+on 
+LOWER( ---lower all letters
+        REGEXP_REPLACE(
+          TRIM(
+            TRANSLATE(cinema.title,
+              'áàäâãåéèëêíìïîóòöôõúùüûñçÁÀÄÂÃÅÉÈËÊÍÌÏÎÓÒÖÔÕÚÙÜÛÑÇ#&@*!?',
+              'aaaaaaeeeeiiiiooooouuuuncAAAAAAEEEEIIIIOOOOOUUUUNC      ' ---remove all the accents from all letters
+            )
+          ),
+          r'[^a-zA-Z0-9\s]', '' ---remove all special characters
+        )
+      ) 
+  =LOWER( ---lower all letters
+        REGEXP_REPLACE(
+          TRIM(
+            TRANSLATE(movie.title,
+              'áàäâãåéèëêíìïîóòöôõúùüûñçÁÀÄÂÃÅÉÈËÊÍÌÏÎÓÒÖÔÕÚÙÜÛÑÇ#&@*!?',
+              'aaaaaaeeeeiiiiooooouuuuncAAAAAAEEEEIIIIOOOOOUUUUNC      ' ---remove all the accents from all letters
+            )
+          ),
+          r'[^a-zA-Z0-9\s]', '' ---remove all special characters
+        )
+      ) )
+select count(*)
+from this
+where director is not null
