1868593c025425e968621d42a9036778a41fa26f
Farzaneh Ariyanpour <ariyanpour.farzaneh@gmail.com>
2025-06-16 09:26:39 +0000

    adding language name to fa_total_cinema and adding duration segment to fa-cinema-director

--- dbt_cinemaparadiso/models/for_analysis/fa_cinema_box_office_director.sql
+++ dbt_cinemaparadiso/models/for_analysis/fa_cinema_box_office_director.sql
@@ -1,4 +1,4 @@
-select cinema.* except(duration),
+WITH total AS (select cinema.* except(duration),
 movie.director AS director,
 COALESCE(cinema.duration, movie.duration) AS duration
 FROM {{ ref('fa_total_cinema_box_office_join') }} AS cinema
@@ -25,4 +25,14 @@ LOWER( ---lower all letters
           ),
           r'[^a-zA-Z0-9\s]', '' ---remove all special characters
         )
-      ) 
+      ) )
+      SELECT *,
+      CASE 
+          WHEN duration >= 180 THEN "Extra long(>=180)"
+          WHEN duration >= 121 AND duration < 180 THEN "long (120-179)"
+          WHEN duration >= 104 AND duration < 121 THEN "Medium long (103-119)"
+          WHEN duration >= 93 AND duration < 104 THEN "Medium short (93-102)"
+          WHEN duration < 93 THEN "Short (=<92)"
+          ELSE "unknown"
+      END AS duration_classification
+      FROM total 
--- dbt_cinemaparadiso/models/for_analysis/fa_total_cinema_box_office_join.sql
+++ dbt_cinemaparadiso/models/for_analysis/fa_total_cinema_box_office_join.sql
@@ -2,5 +2,43 @@ SELECT
     *,
   (
     (rating * votes) + (MIN(votes) OVER () * AVG(rating) OVER ())
-  ) / NULLIF((votes + MIN(votes) OVER ()), 0) AS weighted_score
+  ) / NULLIF((votes + MIN(votes) OVER ()), 0) AS weighted_score,
+  CASE original_language
+    WHEN 'ar' THEN 'Arabic'
+    WHEN 'cn' THEN 'Chinese'
+    WHEN 'cs' THEN 'Czech'
+    WHEN 'da' THEN 'Danish'
+    WHEN 'de' THEN 'German'
+    WHEN 'el' THEN 'Greek'
+    WHEN 'en' THEN 'English'
+    WHEN 'es' THEN 'Spanish'
+    WHEN 'et' THEN 'Estonian'
+    WHEN 'fa' THEN 'Persian'
+    WHEN 'fi' THEN 'Finnish'
+    WHEN 'fr' THEN 'French'
+    WHEN 'hi' THEN 'Hindi'
+    WHEN 'id' THEN 'Indonesian'
+    WHEN 'it' THEN 'Italian'
+    WHEN 'ja' THEN 'Japanese'
+    WHEN 'kn' THEN 'Kannada'
+    WHEN 'ko' THEN 'Korean'
+    WHEN 'ml' THEN 'Malayalam'
+    WHEN 'nl' THEN 'Dutch'
+    WHEN 'no' THEN 'Norwegian'
+    WHEN 'pa' THEN 'Punjabi'
+    WHEN 'pl' THEN 'Polish'
+    WHEN 'pt' THEN 'Portuguese'
+    WHEN 'ru' THEN 'Russian'
+    WHEN 'sr' THEN 'Serbian'
+    WHEN 'sv' THEN 'Swedish'
+    WHEN 'ta' THEN 'Tamil'
+    WHEN 'te' THEN 'Telugu'
+    WHEN 'th' THEN 'Thai'
+    WHEN 'tl' THEN 'Tagalog'
+    WHEN 'tr' THEN 'Turkish'
+    WHEN 'uk' THEN 'Ukrainian'
+    WHEN 'vi' THEN 'Vietnamese'
+    WHEN 'zh' THEN 'Chinese'
+    ELSE 'Unknown'
+  END AS language_name
 FROM {{ ref('ba_total_cinema_box_office_join') }}
